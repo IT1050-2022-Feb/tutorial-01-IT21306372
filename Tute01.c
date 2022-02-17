916b5db6f51518582068b2c3ee08c8f9fd834acb
Inura <99869826+IT21306372@users.noreply.github.com>
2022-02-17 09:01:21 +0000

    Completed

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,19 @@
 #include <stdio.h>
 
 int main() {
+  int num1,num2;
+  float avg,sum;
+  
+  printf("Enter Mark 01 : ");
+  scanf("%d",&num1);
+
+  printf("Enter Mark 02 : ");
+  scanf("%d",&num2);
+
+  sum=num1+num2;
+  avg=sum/2.0;
+
+  printf("Avarage is : %.2f ",avg);
   
   return 0;
 }
