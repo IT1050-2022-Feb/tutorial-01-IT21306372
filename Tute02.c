311b4682ed227a2d3a1669732bc4e684b062aedd
Inura <99869826+IT21306372@users.noreply.github.com>
2022-02-20 17:44:26 +0000

    Completed

--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,18 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
+  int distance;
+  float amount;
+
+  printf("Enter the distance traveld : ");
+  scanf("%d",&distance);
+
+  if(distance<=30){
+    amount=50.00*distance;
+  }else
+    amount=30*50+(distance-30)*40.00;
+  
+  printf("Total amount is : %.2f",amount);
   
   return 0;
 }
